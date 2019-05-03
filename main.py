a35a8237474407bcad2c9f8df7eace717fc2be30
liuzhengwei127 <756670011@qq.com>
2019-05-03 23:21:52 +0800

    the first try to GitPython

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from git import Repo
+import os
 
+dir = os.path.dirname(os.path.abspath(__file__))
+print("目录："+dir)
+repo = Repo(dir)
+if ~repo.bare:
+    fp = open("README.md",'w')
+    git = repo.git
+    git.add(".")
+    git.commit("-m", "the first try to GitPython")

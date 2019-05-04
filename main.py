fc32812296ae5758b885d241e45bbdb83ab4846e
liuzhengwei127 <756670011@qq.com>
2019-05-04 16:32:41 +0800

    have a try to write README.md with localtime and commit it

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from git import Repo
 import os
+import time
 
 dir = os.path.dirname(os.path.abspath(__file__))
 print("目录："+dir)
 repo = Repo(dir)
 if ~repo.bare:
     fp = open("README.md",'a+')
-    fp.write("have a try\n")
+    date = time.strftime('%Y.%m.%d',time.localtime(time.time()))
+    fp.write("date\n")
     git = repo.git
     git.add(".")
-    git.commit("-m", "have a try to write README.md and commit it")
+    git.commit("-m", "have a try to write README.md with localtime and commit it")

aea638f0ceedb995da2f2119fb7cabb854140231
liuzhengwei127 <756670011@qq.com>
2019-05-04 16:24:00 +0800

    have a try to write README.md and commit it

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ dir = os.path.dirname(os.path.abspath(__file__))
 print("目录："+dir)
 repo = Repo(dir)
 if ~repo.bare:
-    fp = open("README.md",'w')
+    fp = open("README.md",'a+')
+    fp.write("have a try\n")
     git = repo.git
     git.add(".")
-    git.commit("-m", "the first try to GitPython")
+    git.commit("-m", "have a try to write README.md and commit it")
